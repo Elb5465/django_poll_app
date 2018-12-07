97069e2aaedb315d9dfd91df81439d152af999c3
Eric Baker <eric.baker@dieboldnixdorf.com>
2018-12-07 12:08:46 -0500

    django polls app

---
+++ polls/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path 
+from . import views 
+
+#SET APP_NAME TO MAKE THESE URL PATTERNS FALL UNDER ITS NAMESPACE (avoids mistaken url patterns from other apps with the same directory names)
+app_name = 'polls'
+urlpatterns = [
+    # ex: /polls/
+    path('', views.index, name='index'),
+    # ex: /polls/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /polls/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
