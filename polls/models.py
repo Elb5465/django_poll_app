90b41570a8171c2f4a6775310bc609f2813adaf6
Eric Baker <eric.baker@dieboldnixdorf.com>
2018-12-07 14:16:20 -0500

    12/7/18 2:15pm Friday

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,7 @@
 from django.db import models 
+import datetime
+from django.utils import timezone
+
 # Create your models here.  
 
 
@@ -9,6 +12,9 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
